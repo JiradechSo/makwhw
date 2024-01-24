18763d332089aaddf16ece2ca3aad5d6130023a2
JiradechSo <jiradech.so@rmuti.ac.th>
2024-01-24 22:23:53 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 require('dotenv').config()
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+const errorHandler = require("./middlewares/error");
+const notFoundHandler = require("./middlewares/notfound");
+const createError = require("./utils/createError")
+
+const authRoute = require("./routers/auth-route")
+const productRoute = require("./routers/product-route")
+
+app.use(cors())
+app.use(express.json());
 
 
+app.use("/auth",authRoute)
+app.use("/product", productRoute)
+
+
+app.use(errorHandler)
+app.use("*",notFoundHandler)
 const port = process.env.PORT
 app.listen(port, () => {
-    console.log("Server run on port " + " " + port);
-});
+    console.log(`Server is running on port ${port}`);
+  });
